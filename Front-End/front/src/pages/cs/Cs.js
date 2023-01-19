34c589682031b84a088ae785f6760d0eb93c8dd8
yunseonyoung <sunysa8@naver.com>
2023-01-19 16:30:55 +0900

    feat : 기본 컴포넌트 라우터 생성 및 주소 연결

---
+++ Front-End/front/src/pages/cs/Cs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Cs() {
+    return (
+        <div className='Cs'>
+            <h1>Cs</h1>
+        </div>
+    )
+}
+
+export default Cs
---
+++ Front-End/front/src/pages/manage/Manage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Manage() {
+    return (
+        <div className='Manage'>
+            <h1>Manage</h1>
+        </div>
+    )
+}
+
+export default Manage
