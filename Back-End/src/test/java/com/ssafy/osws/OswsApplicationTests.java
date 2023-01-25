87e999487babf6f292ea8df2a8745c866abe93f1
김호균 <h0kyungim@gmail.com>
2023-01-25 14:10:44 +0900

    create/back end project

---
+++ Back-End/src/test/java/com/ssafy/osws/OswsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.osws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OswsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
