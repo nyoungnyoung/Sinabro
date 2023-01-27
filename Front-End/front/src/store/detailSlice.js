6304d8f8c86f22416b6390c99274c96ba04fca7e
윤선영 <sunysa8@naver.com>
2023-01-27 17:44:09 +0900

    feat : 상세페이지 하단 라우터 구현중

---
+++ Front-End/front/src/store/detailSlice.js
@@ -0,0 +1,9 @@
+// import { createSlice } from "@reduxjs/toolkit";
+
+// export const detailSlice = createSlice({
+//   name: "detail",
+//   initialState: { value: {} },
+//   reducers: {},
+// });
+
+// export default detailSlice.reducer;
