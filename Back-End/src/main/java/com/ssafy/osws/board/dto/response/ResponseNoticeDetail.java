3882d52da44c65bdbc7fa65b2668d5d5b44fee97
김호균 <h0kyungim@gmail.com>
2023-01-30 15:54:02 +0900

    Resolved S08P12D203-139 "Feature/공지사항 swagger 추가"

---
+++ Back-End/src/main/java/com/ssafy/osws/board/dto/response/ResponseNoticeDetail.java
@@ -0,0 +1,19 @@
+package com.ssafy.osws.board.dto.response;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class ResponseNoticeDetail {
+	private int no;
+	private String subject;
+	private String registeredDate;
+	private String content;
+	private List<ResponseNoticeAttachment> fileList;
+
+}
