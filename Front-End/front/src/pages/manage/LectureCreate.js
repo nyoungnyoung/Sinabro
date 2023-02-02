438fe54d5de6d58c7803814d23ba266d26a4a6f4
yunseonyoung <sunysa8@naver.com>
2023-02-02 14:21:09 +0900

    강사페이지 컴포넌트 구현 중-강의생성

--- Front-End/front/src/pages/manage/LectureCreate.js
+++ Front-End/front/src/pages/manage/LectureCreate.js
@@ -1,5 +1,8 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { ko } from "date-fns/esm/locale";
 
 const StyledDiv = styled.div`
   padding-top: 5vh;
@@ -7,7 +10,18 @@ const StyledDiv = styled.div`
   padding-right: 10vw;
 `;
 
+const StyledDPicker = styled(DatePicker)`
+  width: 230px;
+  height: 30px;
+  padding: 8px 20px;
+  border-radius: 4px;
+  border: 1px solid Gray;
+  /* font-size: 12px; */
+`;
+
 function LectureCreate() {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
   const [lecture, setLecture] = useState({
     poster: "",
     title: "",
@@ -29,15 +43,60 @@ function LectureCreate() {
     ],
   });
 
+  // day, weekPlan 제외 onChange함수
+  const onChange = e => {
+    setLecture({
+      ...lecture,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   const date = new Date().getDate();
   console.log(date);
+  console.log(startDate);
+  console.log(endDate);
 
   return (
     <StyledDiv className="LectureCreate">
       <h1>강의생성</h1>
       <hr />
       <form>
-        <input type="text" name="lectureName" value={lecture.title} />
+        <span>강의명 :</span>
+        <input
+          type="text"
+          name="title"
+          value={lecture.title}
+          onChange={onChange}
+        />
+        <br />
+        <span>최대 수강 인원 :</span>
+        <input
+          type="text"
+          name="maxCapacity"
+          value={lecture.maxCapacity}
+          onChange={onChange}
+        />
+        <br />
+        <span>강의 시작일 :</span>
+        <StyledDPicker
+          selected={startDate}
+          onChange={date => setStartDate(date)}
+          locale={ko}
+          dateFormat="yyyy.MM.dd (eee)"
+          // showPopperArrow={false}
+          minDate={new Date()}
+          popperPlacement="auto"
+        />
+        <span>강의 종료일</span>
+        <StyledDPicker
+          selected={endDate}
+          onChange={date => setEndDate(date)}
+          locale={ko}
+          dateFormat="yyyy.MM.dd (eee)"
+          // showPopperArrow={false}
+          minDate={startDate}
+          popperPlacement="auto"
+        />
       </form>
     </StyledDiv>
   );
