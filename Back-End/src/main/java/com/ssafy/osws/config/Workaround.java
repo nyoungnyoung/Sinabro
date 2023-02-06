9cad286e4dd0eedeb5ef5729e1dd4a4d9fe81628
김호균 <h0kyungim@gmail.com>
2023-02-06 17:03:41 +0900

    feat: swagger 관련 파일

---
+++ Back-End/src/main/java/com/ssafy/osws/config/Workaround.java
@@ -0,0 +1,36 @@
+package com.ssafy.osws.config;
+
+import java.util.Arrays;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Component;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import springfox.documentation.oas.web.OpenApiTransformationContext;
+import springfox.documentation.oas.web.WebMvcOpenApiTransformationFilter;
+import springfox.documentation.spi.DocumentationType;
+
+@Component
+public class Workaround implements WebMvcOpenApiTransformationFilter {
+
+	@Override
+    public OpenAPI transform(OpenApiTransformationContext<HttpServletRequest> context) {
+        OpenAPI openApi = context.getSpecification();
+        Server localServer = new Server();
+        localServer.setDescription("local");
+        localServer.setUrl("http://localhost:5000");
+
+        Server testServer = new Server();
+        testServer.setDescription("test");
+        testServer.setUrl("https://i8d203.p.ssafy.io/api");
+        openApi.setServers(Arrays.asList(localServer, testServer));
+        return openApi;
+    }
+
+    @Override
+    public boolean supports(DocumentationType documentationType) {
+        return documentationType.equals(DocumentationType.OAS_30);
+    }
+}
