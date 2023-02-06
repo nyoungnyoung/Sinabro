4ea3f6721156a6f02dd33356d6fe7c2296d9bb3f
김호균 <h0kyungim@gmail.com>
2023-02-06 15:20:16 +0900

    fix: 중복 japrepository 삭제

--- Back-End/src/main/java/com/ssafy/osws/user/data/repository/LectureTimeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.osws.user.data.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ssafy.osws.user.data.entity.LectureTime;
-
-public interface LectureTimeRepository extends JpaRepository<LectureTime, Integer> {
-
-}
