c766935e591df1ef2a7a04a066c8724b6e883291
이진우 <blu_mountain@naver.com>
2023-02-09 21:59:34 +0900

    refactor: Main API (강의정보 불러오기) 수정

--- Back-End/src/main/java/com/ssafy/osws/main/service/MainService.java
+++ Back-End/src/main/java/com/ssafy/osws/main/service/MainService.java
@@ -2,8 +2,10 @@ package com.ssafy.osws.main.service;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
+import com.ssafy.osws.lecture.dto.response.ResponseLectureDetail;
 import com.ssafy.osws.main.dto.response.ResponseCategory;
-import com.ssafy.osws.main.dto.response.ResponseLecture;
 import com.ssafy.osws.main.dto.response.ResponsePriorityNotice;
 import com.ssafy.osws.main.dto.response.ResponseSubCategory;
 
@@ -11,7 +13,7 @@ public interface MainService {
 	ResponsePriorityNotice getPriorityNotice() throws Exception;
 	List<ResponseCategory> getCategoryList() throws Exception;
 	List<ResponseSubCategory> getSubCategoryList(String categoryNumber);
-	List<ResponseLecture> getLectureListByCategory(String categoryNumber);
-	List<ResponseLecture> getLectureListBySubCategory(String categoryNumber);
-	List<ResponseLecture> searchLectureList(String query);
+	List<ResponseLectureDetail> getLectureListByCategory(String categoryNumber, HttpServletRequest request);
+	List<ResponseLectureDetail> getLectureListBySubCategory(String categoryNumber, HttpServletRequest request);
+	List<ResponseLectureDetail> searchLectureList(String query, HttpServletRequest request);
 }
