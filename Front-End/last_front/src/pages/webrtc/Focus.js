aa4d0d1e7fdc25fce114aeba7f4414166f3deb7b
yunseonyoung <sunysa8@naver.com>
2023-02-14 15:01:49 +0900

    bug: 돋보기 파트 주석처리하여 정상 진행 확인

--- Front-End/last_front/src/pages/webrtc/Focus.js
+++ Front-End/last_front/src/pages/webrtc/Focus.js
@@ -11,98 +11,98 @@ function Focus({
   ratio,
   role,
 }) {
-  const imageRectRef = useRef();
-  let containerDiv = useRef();
+  // const imageRectRef = useRef();
+  // let containerDiv = useRef();
 
-  useEffect(() => {
-    if (
-      containerDiv.current.offsetWidth * ratio +
-        containerDiv.current.offsetLeft <
-      imageRectRef.current.offsetWidth
-    ) {
-      containerDiv.current.style.left = `${
-        imageRectRef.current.offsetWidth -
-        containerDiv.current.offsetHeight * ratio
-      }px`;
-    }
+  // useEffect(() => {
+  //   if (
+  //     containerDiv.current.offsetWidth * ratio +
+  //       containerDiv.current.offsetLeft <
+  //     imageRectRef.current.offsetWidth
+  //   ) {
+  //     containerDiv.current.style.left = `${
+  //       imageRectRef.current.offsetWidth -
+  //       containerDiv.current.offsetHeight * ratio
+  //     }px`;
+  //   }
 
-    if (
-      containerDiv.current.offsetHeight * ratio +
-        containerDiv.current.offsetTop <
-      imageRectRef.current.offsetHeight
-    ) {
-      containerDiv.current.style.top = `${
-        imageRectRef.current.offsetHeight -
-        containerDiv.current.offsetHeight * ratio
-      }px`;
-    }
-  }, [ratio]);
+  //   if (
+  //     containerDiv.current.offsetHeight * ratio +
+  //       containerDiv.current.offsetTop <
+  //     imageRectRef.current.offsetHeight
+  //   ) {
+  //     containerDiv.current.style.top = `${
+  //       imageRectRef.current.offsetHeight -
+  //       containerDiv.current.offsetHeight * ratio
+  //     }px`;
+  //   }
+  // }, [ratio]);
 
-  let posX;
-  let posY;
+  // let posX;
+  // let posY;
 
-  const moveScreenStart = e => {
-    const img = new Image();
-    e.dataTransfer.setDragImage(img, 0, 0);
+  // const moveScreenStart = e => {
+  //   const img = new Image();
+  //   e.dataTransfer.setDragImage(img, 0, 0);
 
-    posX = e.clientX;
-    posY = e.clientY;
-  };
-
-  const moveScreen = e => {
-    const minX = e.target.offsetLeft + (e.clientX - posX) <= 0;
-    const minY = e.target.offsetTop + (e.clientY - posY) <= 0;
-
-    if (
-      e.target.offsetWidth * ratio + e.target.offsetLeft <
-      imageRectRef.current.offsetWidth
-    ) {
-      e.target.style.left = `${
-        imageRectRef.current.offsetWidth - e.target.offsetWidth * ratio
-      }px`;
-    }
-
-    if (
-      e.target.offsetHeight * ratio + e.target.offsetTop <
-      imageRectRef.current.offsetHeight
-    ) {
-      e.target.style.top = `${
-        imageRectRef.current.offsetHeight - e.target.offsetHeight * ratio
-      }px`;
-    }
-
-    if (e.target.offsetLeft + (e.clientX - posX) <= 0) {
-      e.target.style.left = `${e.target.offsetLeft + (e.clientX - posX)}px`;
-    } else {
-      e.target.style.left = `0px`;
-    }
-
-    e.target.style.top = minY
-      ? `${e.target.offsetTop + (e.clientY - posY)}px`
-      : "0px";
-
-    posX = minX ? e.clientX : 0;
-    posY = minY ? e.clientY : 0;
-  };
-
-  const moveScreenEnd = e => {
-    const limitX = e.target.offsetLeft + (e.clientX - posX) <= 0;
-    const limitY = e.target.offsetTop + (e.clientY - posY) <= 0;
-
-    e.target.style.left = limitX
-      ? `${e.target.offsetLeft + (e.clientX - posX)}px`
-      : "0px";
-    e.target.style.top = limitY
-      ? `${e.target.offsetTop + (e.clientY - posY)}px`
-      : "0px";
-  };
-
-  const [over, setOver] = useState(false);
-
-  // const changeToSecond = () => {
-  //   setOver(!over);
-  //   console.log(over);
+  //   posX = e.clientX;
+  //   posY = e.clientY;
   // };
+
+  // const moveScreen = e => {
+  //   const minX = e.target.offsetLeft + (e.clientX - posX) <= 0;
+  //   const minY = e.target.offsetTop + (e.clientY - posY) <= 0;
+
+  //   if (
+  //     e.target.offsetWidth * ratio + e.target.offsetLeft <
+  //     imageRectRef.current.offsetWidth
+  //   ) {
+  //     e.target.style.left = `${
+  //       imageRectRef.current.offsetWidth - e.target.offsetWidth * ratio
+  //     }px`;
+  //   }
+
+  //   if (
+  //     e.target.offsetHeight * ratio + e.target.offsetTop <
+  //     imageRectRef.current.offsetHeight
+  //   ) {
+  //     e.target.style.top = `${
+  //       imageRectRef.current.offsetHeight - e.target.offsetHeight * ratio
+  //     }px`;
+  //   }
+
+  //   if (e.target.offsetLeft + (e.clientX - posX) <= 0) {
+  //     e.target.style.left = `${e.target.offsetLeft + (e.clientX - posX)}px`;
+  //   } else {
+  //     e.target.style.left = `0px`;
+  //   }
+
+  //   e.target.style.top = minY
+  //     ? `${e.target.offsetTop + (e.clientY - posY)}px`
+  //     : "0px";
+
+  //   posX = minX ? e.clientX : 0;
+  //   posY = minY ? e.clientY : 0;
+  // };
+
+  // const moveScreenEnd = e => {
+  //   const limitX = e.target.offsetLeft + (e.clientX - posX) <= 0;
+  //   const limitY = e.target.offsetTop + (e.clientY - posY) <= 0;
+
+  //   e.target.style.left = limitX
+  //     ? `${e.target.offsetLeft + (e.clientX - posX)}px`
+  //     : "0px";
+  //   e.target.style.top = limitY
+  //     ? `${e.target.offsetTop + (e.clientY - posY)}px`
+  //     : "0px";
+  // };
+
+  // const [over, setOver] = useState(false);
+
+  // // const changeToSecond = () => {
+  // //   setOver(!over);
+  // //   console.log(over);
+  // // };
 
   const user = info.subscribers.length;
   console.log("참여자 수: " + user);
